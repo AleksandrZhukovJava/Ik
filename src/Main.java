62401c3a3f1041b4b1649e3373ece02ab98755b5
Александр Жуков <a.zhukov@uzum.com>
2024-06-13 17:32:06 +0200

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
     }
 }
